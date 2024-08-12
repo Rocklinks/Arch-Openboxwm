79dabea5ce379382fbf510545ca4576642452b34
Rocklin K S <rocklin2603@gmail.com>
2024-08-12 19:01:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,25 +8,24 @@ fi
 if ! grep -q "chaotic-aur" /etc/pacman.conf; then
     sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
     sudo pacman-key --lsign-key 3056513887B78AEB
-    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-    sudo pacman -Syu --noconfirm 
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+#    sudo pacman -Syu --noconfirm 
 fi
 
 sudo pacman -S --noconfirm xfce4-panel xfdesktop xfce4-settings xfce4-power-manager xfce4-docklike-plugin bc openbox obconf playerctl picom parcellite numlockx rofi polybar lxappearance betterlockscreen zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search zsh-completions
 
 sudo cp cache/* $HOME/.cache/ -rf
-sudo cp etc/udev/90-backlight.rules /etc/udev/rules.d/
+sudo cp udev/90-backlight.rules /etc/udev/rules.d/
 
 sudo cp usr/bin/networkmanager_dmenu /usr/bin/
 cd chmod +x /usr/bin/networkmanager_dmenu
 
-sudo cp zsh/ $HOME/
 
 sudo cp config/* $HOME/.config/ -rf 
 sudo chmod +x $HOME/.config/polybar/scripts/*
 
-tar -xzvf font/fonts.tar.gz
+tar -xzvf font/Fonts.tar.gz
 sudo mv fonts/fonts/* /usr/share/fonts/ -rf
 sudo fc-cache -fv
 
