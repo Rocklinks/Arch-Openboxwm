f929e52163cf5bb2f40d7e8e4e8fafcb008fac8d
Rocklin K S <rocklin2603@gmail.com>
2024-08-15 16:22:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,21 +20,23 @@ if ! grep -q "chaotic-aur" /etc/pacman.conf; then
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 fi
+
 PACMAN="/etc/pacman.conf"
 CHAOTIC="[chaotic-aur]"
 INCLUDE_LINE="Include = /etc/pacman.d/chaotic-mirrorlist"
 
 # Check if the section already exists in the file
-if ! grep -q "$CHAOTIC_AUR_SECTION" "$PACMAN"; then
-#     Add the section to the end of the file
+if ! grep -q "$CHAOTIC" "$PACMAN"; then
+    # Add the section to the end of the file
     echo -e "\n$CHAOTIC\n$INCLUDE_LINE" >> "$PACMAN"
     echo "Added $CHAOTIC and $INCLUDE_LINE to $PACMAN."
 else
     echo "$CHAOTIC already exists in $PACMAN."
 fi
 
+
 sudo pacman -Syu --noconfirm
-# Define the list of packages to install
+ Define the list of packages to install
 packages=(
     zramswap preload python-dbus auto-cpufreq
     xfce4-panel polkit-gnome xfdesktop blueman
@@ -42,7 +44,7 @@ packages=(
     bc openbox obconf playerctl picom parcellite
     numlockx rofi polybar lxappearance betterlockscreen
     zsh zsh-syntax-highlighting zsh-autosuggestions
-    zsh-history-substring-search zsh-completions
+   zsh-history-substring-search zsh-completions
 )
 
 # Install the packages if they are not already installed
@@ -56,13 +58,10 @@ done
 
 ##Services to Enbale
 sudo systemctl enable --now bluetooth
-sudo systemctl enable --now preload
-
-## Move the betterlock folder
-sudo mv -f cache/* "$HOME/.cache/"
+#sudo systemctl enable --now preload
 
 # Copy the backlight rules file, forcing the overwrite
-sudo mv -f udev/rules.d/90-backlight.rules /etc/udev/rules.d/
+sudo cp -Rf udev/rules.d/90-backlight.rules /etc/udev/rules.d/
 
 # Define the path to the udev rules file
 RULES_FILE="/etc/udev/rules.d/90-backlight.rules"
@@ -70,45 +69,57 @@ CURRENT_USER=$(whoami)
 sudo sed -i "s/\$USER/$CURRENT_USER/g" "$RULES_FILE"
 
 # Copy the networkmanager_dmenu file, forcing the overwrite
-sudo mv -f usr/bin/networkmanager_dmenu /usr/bin/
+sudo cp -Rf usr/bin/networkmanager_dmenu /usr/bin/
 sudo chmod +x /usr/bin/networkmanager_dmenu
 
-sudo mv -f Fonts/ /usr/share/fonts/
+mkdir -p Fonts
+tar -xzvf Fonts.tar.gz -C Fonts
+sudo cp -Rf Fonts/ /usr/share/fonts/
 sudo fc-cache -fv
+
+default_user=$(getent passwd 1000 | cut -d: -f1)
+config_dir="/home/$default_user/.config"
+home_dir="/home/$default_user"
+
+# Create the destination directory if it doesn't exist
+sudo -u "$default_user" mkdir -p "$config_dir"
+
+# Copy the directories
+sudo -u "$default_user" cp -Rf config/dunst config/networkmanager-dmenu config/openbox config/polybar config/xfce4 "$config_dir/"
+
+# Change permissions for polybar scripts
+sudo -u "$default_user" chmod +x "$config_dir/polybar/scripts/"*
 
 # Create the zsh directory and extract the contents of zsh.tar.gz
 mkdir -p zsh
 tar -xzvf zsh.tar.gz -C zsh
-sudo mv -f zsh/.bashrc $HOME/.bashrc
-sudo mv -f zsh/.zshrc $HOME/.zshrc
+sudo -u "$default_user" cp -Rf zsh/.bashrc "$home_dir/.bashrc"
+sudo -u "$default_user" cp -Rf zsh/.zshrc "$home_dir/.zshrc"
+sudo -u "$default_user" cp -Rf cache/* "$home_dir/.cache/"
 
-# Check if the betterlockscreen cache directory exists and has the correct permissions
-if [ -d "$HOME/.cache/betterlockscreen" ]; then
-    if [ "$(stat -c '%U:%G' "$HOME/.cache/betterlockscreen")" != "root:$(id -gn)" ] || [ "$(stat -c '%a' "$HOME/.cache/betterlockscreen")" != "750" ]; then
-        sudo chown root:$(id -gn) "$HOME/.cache/betterlockscreen"
-        sudo chmod 750 "$HOME/.cache/betterlockscreen"
- 
-    else
-        echo "Permissions for $HOME/.cache/betterlockscreen are already correct. Skipping."
-    fi
-else
-    echo "$HOME/.cache/betterlockscreen directory does not exist. Skipping permission update."
-fi
 
-## Set the permissions for the .config
-sudo chown -R root:$(id -gn) "$HOME/.config"
-chmod -R 770 "$HOME/.config"
+SYSTEM_CONFIG="$home_dir/.config/polybar/system.ini"
+POLYBAR_CONFIG="$home_dir/.config/polybar/config.ini"
 
-## Move the Config files and set the network
-sudo mv -f config/* $HOME/.config/
-sudo chmod +x $HOME/.config/polybar/scripts/*
-CONFIG="$HOME/.config/polybar/config.ini"
+# Get the active Ethernet and Wi-Fi interfaces
+ETHERNET=$(ip link | awk '/state UP/ && !/wl/ {print $2}' | tr -d :)
+WIFI=$(ip link | awk '/state UP/ && /wl/ {print $2}' | tr -d :)
 
-ETHERNET=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
-WIFI=$(ip link | awk '/state UP/ {print $2}' | tr -d : | grep -i '^wl')
-
+# Check if Wi-Fi is active
 if [ -n "$WIFI" ]; then
-    sed -i "s/sys_network_interface = wlan0/sys_network_interface = $WIFI/" "$CONFIG"
+    echo "Using Wi-Fi interface: $WIFI"
+    # Replace wlan0 with the actual Wi-Fi interface name in system.ini
+    sed -i "s/sys_network_interface = wlan0/sys_network_interface = $WIFI/" "$SYSTEM_CONFIG"
+    
+# Check if Ethernet is active
 elif [ -n "$ETHERNET" ]; then
-    sed -i "s/sys_network_interface = wlan0/sys_network_interface = $ETHERNET/" "$CONFIG"
+    echo "Using Ethernet interface: $ETHERNET"
+    # Replace wlan0 with the Ethernet interface name in system.ini
+    sed -i "s/sys_network_interface = wlan0/sys_network_interface = $ETHERNET/" "$SYSTEM_CONFIG"
+    
+    # Replace 'network' with 'ethernet' in config.ini
+    sed -i "s/network/ethernet/g" "$POLYBAR_CONFIG"
+
+else
+    echo "No active network interfaces found."
 fi
