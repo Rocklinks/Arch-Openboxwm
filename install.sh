457046ce0ac6f125c690a803ab89d4b177350072
Rocklin K S <rocklin2603@gmail.com>
2024-09-07 11:05:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -125,4 +125,4 @@ elif [ -n "$ETHERNET" ]; then
 else
     echo "No active network interfaces found."
 fi
-chmod u+rw /sys/class/backlight/intel_backlight/brightness
+sudo chmod 664 /sys/class/backlight/intel_backlight/brightness
