d4ed868d835ec334775d45eecf9856744af6bf07
Rocklin K S <rocklin2603@gmail.com>
2024-09-14 15:46:49 +0530

    Add files via upload

--- bat.sh
+++ bat.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+THRESHOLD=98
 
-# Define the battery device
-BATTERY_DEVICE="/org/freedesktop/UPower/devices/battery_BAT0"
+stop_charging() {
 
-# Get the battery percentage
-charge=$(upower -i $BATTERY_DEVICE | grep percentage | awk '{print $2}' | sed 's/%//')
+    BATTERY_LEVEL=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed s/%//)
 
-# Check if the charge is 100%
-if [ "$charge" -eq 99 ]; then
-    notify-send --icon=/usr/share/icons/kora/devices/scalable/gnome-dev-battery.svg "Battery Status" "Your battery is Almost fully charged"
-fi
+    STATUS=$(acpi -b | grep -o 'Charging\|Discharging\|Full')
+
+    if [ "$BATTERY_LEVEL" -ge "$THRESHOLD" ] && [ "$STATUS" == "Charging" ]; then
+        echo "Battery at $BATTERY_LEVEL%, stopping charging."
+        notify-send --icon=/usr/share/icons/kora/apps/scalable/battery.svg "Battery Info" "Your battery is Almost full"
+
+    else
+        :
+    fi
+}
+
+while true; do
+    stop_charging
+    sleep 20  # Check every 20 Secs
+done
 
